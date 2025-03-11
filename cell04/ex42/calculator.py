c88ac38f757517c39563adb73da1b2e5f7be0d2f
HumanDisaster888 <pajaree2550toon@gmail.com>
2025-03-11 14:51:53 +0700

    Add files via upload

---
+++ cell04/ex42/calculator.py
@@ -0,0 +1,9 @@
+q = int(input("Give me the first number :"))
+r = int(input("Give me teh second number :"))
+
+print(input("Thanks you!"))
+
+print(f"{q} + {r} = {q+r}")
+print(f"{q} - {r} = {q-r}")
+print(f"{q} / {r} = {q/r}")
+print(f"{q} * {r} = {q*r}")

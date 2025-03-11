7614e29ea3568b440ccbaacfc9b81342edd8f2ae
HumanDisaster888 <pajaree2550toon@gmail.com>
2025-03-11 13:44:21 +0700

    Add files via upload

---
+++ cell03/ex32/multiplication_table.py
@@ -0,0 +1,10 @@
+c = str(input("What you gotta say :"))
+t = str(input("I got that! Anything else? :"))
+
+while t != "STOP":
+    t = str(input("I got that! Anything else? :"))
+
+    if t == "STOP":
+        break # stop the loop
+   
+    

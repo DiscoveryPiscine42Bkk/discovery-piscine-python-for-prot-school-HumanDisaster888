07035939237c9fe47c834086905820fbe8c656a6
HumanDisaster888 <pajaree2550toon@gmail.com>
2025-03-11 11:11:09 +0700

    Add files via upload

---
+++ cell03/ex30/25.py
@@ -0,0 +1,8 @@
+x = int(input("Enter a number less than25 :")).__int__()
+y = 25
+if x > 15 :
+    print("Error")
+    
+while x < 26:
+        print(f"inside the loop, my varaible is {x}")
+        x += 1

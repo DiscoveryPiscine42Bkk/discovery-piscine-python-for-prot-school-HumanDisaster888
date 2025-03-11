d365798307902fcaf6084f3f73cf59d3e4ed8ab6
HumanDisaster888 <pajaree2550toon@gmail.com>
2025-03-11 10:03:56 +0700

    Add files via upload

---
+++ cell02/ex23/mult.py
@@ -0,0 +1,19 @@
+c = int(input("Enter teh first number :")).__int__()
+t = int(input("Enter the second number :")).__int__()
+
+x = c*t
+
+if x > 0 :
+    print("positive")
+    print(x)
+
+elif x < 0 :
+    print("Negative")
+    print(x)
+else :
+    print("0")
+    print(x)
+
+
+
+
